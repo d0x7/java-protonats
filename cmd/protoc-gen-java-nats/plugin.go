fe4d6f5a47f3691bc7229f3c767121edc97c2ce5
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2025-02-04 03:54:21 +0100

    Update to be same as go generator

--- cmd/protoc-gen-java-nats/plugin.go
+++ cmd/protoc-gen-java-nats/plugin.go
@@ -4,10 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/pluginpb"
 	"os"
 	"path/filepath"
 )
+
+var protoFiles map[protoreflect.FileDescriptor]*protogen.File
+
+func getProtoFile(method *protogen.Message) *protogen.File {
+	file, ok := protoFiles[method.Desc.ParentFile()]
+	if !ok {
+		panic(fmt.Sprintf("File not found for method %v", method.Desc.FullName()))
+	}
+	return file
+}
 
 func main() {
 	var showVersion bool
@@ -25,7 +36,12 @@ func main() {
 	protogen.Options{
 		ParamFunc: flags.Set,
 	}.Run(func(gen *protogen.Plugin) error {
-		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
+		protoFiles = make(map[protoreflect.FileDescriptor]*protogen.File)
+		for _, file := range gen.Files {
+			protoFiles[file.Desc] = file
+		}
+
 		for _, f := range gen.Files {
 			if f.Generate {
 				generateFile(gen, f)
